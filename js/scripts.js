045913b498c0a886c02d75af4cbc899ba9e5653a
Cameron Jensen and Nathaniel Meyer <cameron@>
2017-02-23 10:35:03 -0800

    Add support for 10-99

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,61 @@ var arabicNumber = 0;
 var romanNumber = "";
 
 var romanNumberConverter = function(arabicNumber) {
+  if (arabicNumber > 9) {
+    tensConverter(arabicNumber);
+  } else {
+    onesConverter(arabicNumber);
+  }
+  return romanNumber;
+};
+
+var tensConverter = function(arabicNumber) {
+  if (arabicNumber >= 90) {
+    romanNumber = romanNumber.concat("XC");
+    arabicNumber -= 90;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 80) {
+    romanNumber = romanNumber.concat("LXXX");
+    arabicNumber -= 80;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 70) {
+    romanNumber = romanNumber.concat("LXX");
+    arabicNumber -= 70;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 60) {
+    romanNumber = romanNumber.concat("LX");
+    arabicNumber -= 60;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 50) {
+    romanNumber = romanNumber.concat("L");
+    arabicNumber -= 50;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 40) {
+    romanNumber = romanNumber.concat("XL");
+    arabicNumber -= 40;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 30) {
+    romanNumber = romanNumber.concat("XXX");
+    arabicNumber -= 30;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 20) {
+    romanNumber = romanNumber.concat("XX");
+    arabicNumber -= 20;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 10) {
+    romanNumber = romanNumber.concat("X");
+    arabicNumber -= 10;
+    onesConverter(arabicNumber);
+  } else if (arabicNumber >= 0) {
+    romanNumber = romanNumber.concat("");
+    onesConverter(arabicNumber);
+  } else {
+    romanNumber = "Rilassati, ci stiamo lavorando!"
+  }
+  return romanNumber;
+};
+
+var onesConverter = function(arabicNumber) {
   if (arabicNumber === 9) {
     romanNumber = romanNumber.concat("IX");
   } else if (arabicNumber === 8) {

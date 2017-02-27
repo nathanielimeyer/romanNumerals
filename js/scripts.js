0ab0f5c1effd79410e4c3b75fa278c41268b4130
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-02-26 16:07:44 -0800

    Refactor and tidy up

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,28 @@
-// Business logic
-var arabicNumber = 0;
-var romanNumber = "";
 var arabicHash = [5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 var romanHash = ["\u2181", "M\u2181", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 var romanNumberConverter = function(arabicNumber, n) {
-  if (arabicNumber === 0) {
-    return romanNumber;
-  }
+  if (arabicNumber === 0) {return romanNumber;}
   else if (arabicNumber >= arabicHash[n]) {
     romanNumber = romanNumber.concat(romanHash[n]);
     arabicNumber -= arabicHash[n];
-    romanNumberConverter(arabicNumber, n);
-  } else {
-    romanNumberConverter(arabicNumber, n+1);
-  }
+    romanNumberConverter(arabicNumber, n);}
+  else {romanNumberConverter(arabicNumber, n+1);}
   return romanNumber;
 };
-// Front end logic
+
 $(function() {
   $("form#userInput").submit(function(event){
     event.preventDefault();
-    arabicNumber = parseInt($("input#numberInput").val());
-    if (!arabicNumber) {
-      $("#errorFeedback").show();
-    } else {
-      $("#errorFeedback").hide();
+    romanNumber = "";
+    $("p").hide();
+    var arabicNumber = Math.abs(parseInt($("input#numberInput").val()));
+    if (!arabicNumber) {$("#errorFeedback").show();}
+    else if (arabicNumber > 8999) {$("#sizeFeedback").show();}
+    else  {
       output = romanNumberConverter(arabicNumber, 0);
       $(".answer").show();
       $("#arabicNumber").text(arabicNumber);
       $("#romanNumber").text(output);
     }
-    romanNumber = "";
   });
 });
